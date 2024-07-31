e721fbb4479944e36e63817c480edd7c2b553061
Contrabang <91113370+Contrabang@users.noreply.github.com>
2024-07-31 13:09:07 -0700

    improves zTXt support

--- waiter.py
+++ waiter.py
@@ -2,6 +2,9 @@ import os
 import sys
 import time
 import struct
+import zlib
+import tempfile
+import shutil
 
 PNG_SIGNATURE = b'\x89PNG\r\n\x1a\n'
 CHUNK_SIZE_LENGTH = 4
@@ -11,6 +14,12 @@ CHUNK_HEADER_LENGTH = CHUNK_SIZE_LENGTH + CHUNK_TYPE_LENGTH
 
 ZTXT_CHUNK_TYPE = b'zTXt'
 IHDR_CHUNK_TYPE = b'IHDR'
+
+def extract_keyword_text(png_path):
+    return decode_ztxt(extract_ztxt_chunk(png_path)[2])
+
+def insert_keyword_text(png_path, keyword, text):
+    replace_ztxt_chunk(png_path, encode_ztxt(keyword, text))
 
 def extract_ztxt_chunk(png_path):
     with open(png_path, 'rb') as f:
@@ -24,11 +33,12 @@ def extract_ztxt_chunk(png_path):
             if not chunk_size_bytes:
                 break
 
-            chunk_size = struct.unpack('!I', chunk_size_bytes)[0]
+            chunk_size = struct.unpack('!I', chunk_size_bytes)[0] # network (big endian), unsigned int
             chunk_type = f.read(CHUNK_TYPE_LENGTH)
 
             if chunk_type == ZTXT_CHUNK_TYPE:
                 chunk_data = f.read(chunk_size+CRC_LENGTH)
+                # chunk_data = keyword, text, crc
                 return (chunk_size_bytes, chunk_type, chunk_data)
 
             f.seek(chunk_size + CRC_LENGTH, os.SEEK_CUR)
@@ -36,9 +46,95 @@ def extract_ztxt_chunk(png_path):
     print('Error: zTXt chunk not found in the PNG')
     return
 
-def insert_ztxt_chunk(dmi_path, png_path, ztxt_chunk_data):
+def decode_ztxt(chunk_data):
+    # Find the null separator to split the keyword and the compressed text
+    null_separator_index = chunk_data.find(b'\x00')
+    if null_separator_index == -1:
+        raise ValueError("Invalid zTXT chunk: No null separator found")
+    
+    # Extract the keyword
+    keyword = chunk_data[:null_separator_index].decode('latin-1')
+    
+    # The compression method is the byte right after the null separator
+    compression_method = chunk_data[null_separator_index + 1]
+    if compression_method != 0:
+        raise ValueError("Unsupported compression method: {}".format(compression_method))
+    
+    # The rest is the compressed text data
+    compressed_text_data = chunk_data[null_separator_index + 2:]
+    
+    # Decompress the text data using zlib. Automatically discards the CRC.
+    decompressed_text_data = zlib.decompress(compressed_text_data)
+    
+    # Decode the decompressed text to a string
+    text = decompressed_text_data.decode('latin-1')
+    
+    return keyword, text
 
+def __keyword_text_to_bytes(keyword, text):
+    if '\x00' in keyword:
+        raise ValueError("Keyword cannot contain null character (\\x00)")
+    
+    # Compress the text data using zlib
+    compressed_text_data = zlib.compress(text.encode('latin-1'))
+    
+    # Construct the zTXT chunk data
+    chunk_data = bytearray()
+    chunk_data.extend(keyword.encode('latin-1'))
+    chunk_data.append(0)  # Null separator
+    chunk_data.append(0)  # Compression method (0 for zlib)
+    chunk_data.extend(compressed_text_data)
+    
+    return bytes(chunk_data)
 
+def encode_ztxt(keyword: str, text: str):
+    encoded = __keyword_text_to_bytes(keyword, text)
+    chunk_size_bytes = struct.pack("!I", len(encoded)) # but the size of the data only includes encoded, not chunk type
+    chunk_type = ZTXT_CHUNK_TYPE
+    new_crc = struct.pack('!I', zlib.crc32(chunk_type+encoded)) # Yes, CRC includs chunk type but not chunk size
+    chunk_data = bytes(encoded + new_crc)
+
+    return (chunk_size_bytes, chunk_type, chunk_data)
+
+def replace_ztxt_chunk(dmi_path, ztxt_chunk_data):
+    # ztxt_chunk_data = list(chunk_size_bytes, chunk_type, chunk_data)
+    # chunk_data = keyword, text, crc
+    
+    with open(dmi_path, 'rb') as f_in:
+        buffer = tempfile.TemporaryFile()
+        shutil.copyfileobj(f_in, buffer)
+        buffer.seek(0)
+
+    with open(dmi_path, 'wb') as f_out:
+        signature = buffer.read(len(PNG_SIGNATURE))
+        if signature != PNG_SIGNATURE:
+            print('Error: Invalid PNG file')
+            return False
+        f_out.write(signature)
+
+        while True:
+            chunk_size_bytes = buffer.read(CHUNK_SIZE_LENGTH)
+            if not chunk_size_bytes:
+                break
+
+            chunk_size = struct.unpack('!I', chunk_size_bytes)[0] # network (big endian), unsigned int
+            chunk_type = buffer.read(CHUNK_TYPE_LENGTH)
+
+            if chunk_type == ZTXT_CHUNK_TYPE:
+                buffer.seek(chunk_size + CRC_LENGTH, os.SEEK_CUR)
+                continue
+            f_out.write(chunk_size_bytes)
+            f_out.write(chunk_type)
+            chunk_data = buffer.read(chunk_size + CRC_LENGTH)
+            f_out.write(chunk_data)
+            if chunk_type == IHDR_CHUNK_TYPE:
+                f_out.write(ztxt_chunk_data[0])
+                f_out.write(ztxt_chunk_data[1])
+                f_out.write(ztxt_chunk_data[2])
+    return True
+
+def _insert_ztxt_chunk(dmi_path, png_path, ztxt_chunk_data):
+    # its not advised to usee this outside of this file, use replace_ztxt_chunk instead
     with open(png_path, 'rb') as f_in, open(dmi_path, 'wb') as f_out:
         signature = f_in.read(len(PNG_SIGNATURE))
         if signature != PNG_SIGNATURE:
@@ -47,14 +143,12 @@ def insert_ztxt_chunk(dmi_path, png_path, ztxt_chunk_data):
 
         f_out.write(signature)
 
-        wrote_ztxt = False
-
         while True:
             chunk_size_bytes = f_in.read(CHUNK_SIZE_LENGTH)
             if not chunk_size_bytes:
                 break
 
-            chunk_size = struct.unpack('!I', chunk_size_bytes)[0]
+            chunk_size = struct.unpack('!I', chunk_size_bytes)[0] # network (big endian), unsigned int
             chunk_type = f_in.read(CHUNK_TYPE_LENGTH)
 
             if chunk_type == ZTXT_CHUNK_TYPE:
@@ -100,7 +194,7 @@ def watch_for_edits(dmi_path):
             if modified > last_modified:
                 print('PNG file modified')
 
-                if insert_ztxt_chunk(dmi_path, png_path, ztxt_chunk_data):
+                if _insert_ztxt_chunk(dmi_path, png_path, ztxt_chunk_data):
                     print('zTXt chunk inserted back into the DMI')
                 else:
                     print('Error: Failed to insert zTXt chunk')
--- wallifer.py
+++ wallifer.py
@@ -1,15 +1,12 @@
 from PIL import Image
 import os
 import json
-
 import waiter
-
 import shutil
     
 global_possible_states = list(range(16)) + [21, 23, 29, 31, 38, 39, 46, 47, 55, 63, 74, 75, 78, 79, 95, 110, 111, 127, 137, 139, 141, 143, 157, 159, 175, 191, 203, 207, 223, 239, 255]
 
 def handle_shit(png_path, dmi_path, name):
-
     with open("data/output.json", 'r') as file:
         data = json.load(file)
 
@@ -35,16 +32,15 @@ def handle_shit(png_path, dmi_path, name):
     }
 
     dmi_file = Image.open(dmi_path)
-    # saved_text = dmi_file.text
     dmi_file = dmi_file.convert('RGBA') # conversion to RGBA
-    DMI_pixels = dmi_file.load()
+    # DMI_pixels = dmi_file.load()
 
     size_of_input = tuple([dmi_file.size[0] // 32, dmi_file.size[1] // 32])
-    total_idx = (size_of_input[0] * size_of_input[1]) - 1
+    # total_idx = (size_of_input[0] * size_of_input[1]) - 1
 
-    final_idx = 0
+    # final_idx = 0
 
-    img = Image.new('RGBA', dmi_file.size)
+    img = Image.new('RGBA', (dmi_file.size[0], int(dmi_file.size[1]*1.5)))
     # img = img.convert('RGBA') # conversion to RGBA
     PixelsFinal = img.load()
 
@@ -52,7 +48,7 @@ def handle_shit(png_path, dmi_path, name):
         target_offset = data[str(state)]
 
         state_x_offset = (idx % size_of_input[0]) * 32
-        state_y_offset = (idx // size_of_input[1]) * 32
+        state_y_offset = (idx // size_of_input[1]) * 48
 
         # get_sprite(SourcePixels, target_offset, state)
         for areas, our_offset in zip(amounts, target_offset):
@@ -61,32 +57,35 @@ def handle_shit(png_path, dmi_path, name):
                 output_x = i + state_x_offset
                 for j in areas[1]:
                     input_y = j + global_offsets[our_offset][1]
-                    output_y = j + state_y_offset
+                    output_y = j + state_y_offset + 16
                     PixelsFinal[output_x,output_y] = SourcePixels[input_x,input_y]
 
-        final_idx = idx
+        # final_idx = idx
 
-    new_idx = final_idx + 1
-    while (new_idx < total_idx):
-        state_x_offset = (new_idx % size_of_input[0]) * 32
-        state_y_offset = (new_idx // size_of_input[1]) * 32
-        for i in range(32):
-            i += state_x_offset
-            for j in range(32):
-                j += state_y_offset
-                PixelsFinal[i,j] = DMI_pixels[i,j]
+    # This used to handle fake sliding walls, but we had to ditch it because it broke when we changed to 32x48
+    # new_idx = final_idx + 1
+    # while (new_idx < total_idx):
+    #     state_x_offset = (new_idx % size_of_input[0]) * 32
+    #     state_y_offset = (new_idx // size_of_input[1]) * 32
+    #     for i in range(32):
+    #         i += state_x_offset
+    #         for j in range(32):
+    #             j += state_y_offset
+    #             PixelsFinal[i,j] = DMI_pixels[i,j]
 
-        new_idx += 1
+    #     new_idx += 1
 
     # img.text = dmi_file.text
     output_file = f"./output/{name}.png"
     img.save(output_file)
     img.close()
 
-    ztxt_chunk_data = waiter.extract_ztxt_chunk(dmi_path)
+    keyword, text = waiter.extract_keyword_text(dmi_path)
+    text = text.replace("height = 32", "height = 48")
+    reecoded_chunk_data = waiter.encode_ztxt(keyword, text)
     new_file_path = os.path.splitext(output_file)[0] + ".dmi"
     shutil.copy2(output_file, new_file_path)
-    waiter.insert_ztxt_chunk(new_file_path, output_file, ztxt_chunk_data)
+    waiter.insert_ztxt_chunk(new_file_path, output_file, reecoded_chunk_data) # fun fact, PIL can't write zTXt :)
 
     os.remove(output_file)
 
@@ -104,12 +103,10 @@ def begin(wall_folder):
         dmi_path = os.path.join(wall_folder, f"{match}.dmi")
 
         handle_shit(png_path, dmi_path, match)
-    
 
 if __name__ == "__main__":
     # reset_bases()
 
-    # MCRAMON, CHANGE THIS PATH TO THE PATH OF YOUR PARADISE WALLS FOLDER
-    wall_folder = r"C:\Users\Bluecloud\Documents\github\Paradise\icons\turf\walls"
-
+    # CHANGE THIS PATH TO THE PATH OF YOUR PARADISE WALLS FOLDER
+    wall_folder = r"C:\Users\YOUR_USERNAME\Documents\github\Paradise\icons\turf\walls"
     begin(wall_folder)
